53365d88d51ce22075a395a3fd8df78d5f7f088e
Hyedora <byj4565@kaist.ac.kr>
2020-07-20 14:56:51 +0900

    loading mat files

--- Building_ref1.py
+++ Building_ref1.py
@@ -1,1 +1,16 @@
-import numpy
+import os
+from scipy import io
+
+def load_mat_files(dataDir):
+    mats = []
+    for file in os.listdir(dataDir):
+        mats.append(io.loadmat(dataDir+file)['gestures'])
+        
+    return mats
+
+
+def main():
+    mats = load_mat_files("../data/ref1_subject1_session1/")
+
+
+main()
