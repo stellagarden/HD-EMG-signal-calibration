a7b3aaf5a7eac215c44af89f89c102fd7ae04486
Hyedora <byj4565@kaist.ac.kr>
2020-07-22 14:57:37 +0900

    Segmentation : organized RMS values [Complete]

--- Building_ref1.py
+++ Building_ref1.py
@@ -49,6 +49,22 @@ def compute_RMS_for_each_windows(windows):
         RMSs=np.append(RMSs, [[compute_RMS(window)]], axis=0)
     return RMSs
 
+def create_168_dimensional_window_vectors(one_try):
+    for i in range(len(one_try[0])):
+        if (i-1)%8 == 0:
+            continue
+        # Preprocessing : Applying Fourth order butterworth band-pass filter (20-400Hz)
+        filtered_channel=butter_bandpass_filter(one_try[0][i])
+        # Segmentation (1) Construct windows
+        windows_per_channel=divide_to_windows(filtered_channel)
+        # Segmentation (2) Compute RMS for each windows
+        RMSwindows_per_channel=compute_RMS_for_each_windows(windows_per_channel)
+        if i==0:
+            RMS_one_try=np.array(RMSwindows_per_channel)
+            continue
+        RMS_one_try=np.append(RMS_one_try, RMSwindows_per_channel, axis=1)
+    return RMS_one_try
+
 
 def main():
     #loading .mat files consist of 0,1,2,3,11,17,18,21,23,24,25 gestures
@@ -56,27 +72,27 @@ def main():
     #In idle gesture, we just use 2,4,7,8,11,13,19,25,26,30th tries in order to match the number of datas
     gestures[0]=gestures[0][[1,3,6,7,10,12,18,24,25,29]]
 
+    init_gesture=1
     for gesture in gestures:
+        init_try=1
         for one_try in gesture:
-            for i in range(len(one_try[0])):
-                if (i-1)%8 == 0:
-                    continue
-                # Preprocessing : Applying Fourth order butterworth band-pass filter (20-400Hz)
-                filtered_channel=butter_bandpass_filter(one_try[0][i])
-                # Segmentation (1) Construct windows
-                windows_per_channel=divide_to_windows(filtered_channel)
-                # Segmentation (2) Compute RMS for each window
-                RMSwindows_per_channel=compute_RMS_for_each_windows(windows_per_channel)
-                if i==0:
-                    RMSwindows=np.array(RMSwindows_per_channel)
-                    continue
-                RMSwindows=np.append(RMSwindows, RMSwindows_per_channel, axis=1)
-                break
-            break
-        break
+            RMS_one_try = create_168_dimensional_window_vectors(one_try)
+            if init_try == 1:
+                RMS_tries_for_gesture = np.array([RMS_one_try])
+                init_try=0
+                continue
+            RMS_tries_for_gesture = np.append(RMS_tries_for_gesture, [RMS_one_try], axis=0)
 
-    return RMSwindows
-                
+        if init_gesture==1:
+            RMS_gestures = np.array([RMS_tries_for_gesture])
+            init_gesture=0
+            continue
+        RMS_gestures = np.append(RMS_gestures, [RMS_tries_for_gesture], axis=0)
+    
+    print(len(RMS_gestures))
+    print(len(RMS_gestures[0]))
+    print(len(RMS_gestures[0][0]))
+    print(len(RMS_gestures[0][0][0]))
 
 
-print(main())
+main()
