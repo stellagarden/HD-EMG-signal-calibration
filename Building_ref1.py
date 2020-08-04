6212bab132b3d4a7870858082d596be9968815b1
Hyedora <byj4565@kaist.ac.kr>
2020-08-04 17:15:44 +0900

    .

--- Building_ref1.py
+++ Building_ref1.py
@@ -194,4 +194,5 @@ def main():
     ACTIVE_RMS_gestures=ACTIVE_filter(RMS_gestures.tolist())
     # Feature extraction : Mean normalization for all channels in each window
     mean_normalized_RMS=mean_normalization(np.array(ACTIVE_RMS_gestures))
+    
 main()
