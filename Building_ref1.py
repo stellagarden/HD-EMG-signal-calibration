fa757f7beeec39aa2bf32532eb846147ceecaacb
Hyedora <byj4565@kaist.ac.kr>
2020-10-06 21:17:29 +0900

    shorten libraries

--- Building_ref1.py
+++ Building_ref1.py
@@ -1,11 +1,10 @@
-import os
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-import random
-import pandas as pd
-import glob
-import time
+from pandas import DataFrame
+from glob import glob
+from time import time
+from random import randint
+from seaborn import heatmap
 from mpl_toolkits import mplot3d
 from scipy import io
 from scipy.signal import butter, lfilter, freqz
@@ -20,19 +19,21 @@ from sklearn.mixture import GaussianMixture
 WINDOW_SIZE = 150    # 20:9.76ms, 150:73.2ms
 TEST_RATIO = 0.3
 SEGMENT_N = 3
-PLOT_RANDOM_DATA = False
-PLOT_CONFUSION_MATRIX = True
 ACTUAL_COLUMN=24
 ACTUAL_RAW=7
+
+PLOT_RANDOM_DATA = False
 PLOT_PRINT_PROCESSING = False
 PRINT_TIME_CONSUMING = False
+GMM_CALIBRATE = False
 GNB_CLASSIFY = True
-GMM_CLASSIFY = False
+PLOT_CONFUSION_MATRIX = True
+
 
 def load_mat_files(dataDir):
-    if PRINT_TIME_CONSUMING: t_load_mat_files=time.time()
+    if PRINT_TIME_CONSUMING: t_load_mat_files=time()
     pathname=dataDir + "/**/*.mat"
-    files = glob.glob(pathname, recursive=True)
+    files = glob(pathname, recursive=True)
     sessions=dict()
     #In idle gesture, we just use 2,4,7,8,11,13,19,25,26,30th tries in order to match the number of datas
     for one_file in files:
@@ -46,7 +47,7 @@ def load_mat_files(dataDir):
             sessions[session_name]=np.append(sessions[session_name], [io.loadmat(one_file)['gestures'][[1,3,6,7,10,12,18,24,25,29]]], axis=0)
             continue
         sessions[session_name]=np.append(sessions[session_name], [io.loadmat(one_file)['gestures']], axis=0)
-    if PRINT_TIME_CONSUMING: print("Loading mat files: %.2f" %(time.time()-t_load_mat_files))
+    if PRINT_TIME_CONSUMING: print("Loading mat files: %.2f" %(time()-t_load_mat_files))
     return sessions
 
 def butter_bandpass_filter(data, lowcut=20.0, highcut=400.0, fs=2048, order=4):
@@ -70,15 +71,15 @@ def compute_RMS_gestures(gestures):
     return RMS_gestures
 
 def base_normalization(RMS_gestures):
-    if PRINT_TIME_CONSUMING: t_base_normalization=time.time()
+    if PRINT_TIME_CONSUMING: t_base_normalization=time()
     # Compute mean value of each channel of idle gesture
     average_channel_idle_gesture=np.mean(np.mean(RMS_gestures[0], 2), 0)
     # Subtract above value from every channel
-    if PRINT_TIME_CONSUMING: print("## base_normalization: %.2f" %(time.time()-t_base_normalization))
+    if PRINT_TIME_CONSUMING: print("## base_normalization: %.2f" %(time()-t_base_normalization))
     return np.transpose(np.transpose(RMS_gestures,(0,1,3,2))-average_channel_idle_gesture,(0,1,3,2))
 
 def extract_ACTIVE_window_i(RMS_gestures):
-    if PRINT_TIME_CONSUMING: t_extract_ACTIVE_window_i=time.time()
+    if PRINT_TIME_CONSUMING: t_extract_ACTIVE_window_i=time()
     RMS_gestures=np.transpose(RMS_gestures,(0,1,3,2))
     ## Determine whether ACTIVE : Compute summarized RMS
     sum_RMSs=np.sum(RMS_gestures,3)
@@ -104,7 +105,7 @@ def extract_ACTIVE_window_i(RMS_gestures):
                         contiguous=0
             i_ACTIVE_windows[i_ges][i_try][0]=MAX_start
             i_ACTIVE_windows[i_ges][i_try][1]=MAX_contiguous
-    if PRINT_TIME_CONSUMING: print("## extract_ACTIVE_window_i: %.2f" %(time.time()-t_extract_ACTIVE_window_i))
+    if PRINT_TIME_CONSUMING: print("## extract_ACTIVE_window_i: %.2f" %(time()-t_extract_ACTIVE_window_i))
     return np.array(i_ACTIVE_windows)
 
 def medfilt(channel, kernel_size=3):
@@ -117,17 +118,17 @@ def medfilt(channel, kernel_size=3):
 
 def ACTIVE_filter(i_ACTIVE_windows, gestures):
     # ACTIVE_filter : delete if the window is not ACTIVE
-    if PRINT_TIME_CONSUMING: t_ACTIVE_filter=time.time()
+    if PRINT_TIME_CONSUMING: t_ACTIVE_filter=time()
     list_gestures=np.transpose(gestures, (0,1,3,2,4)).tolist()
     for i_ges in range(i_ACTIVE_windows.shape[0]):
         for i_try in range(i_ACTIVE_windows.shape[1]):
             del list_gestures[i_ges][i_try][:i_ACTIVE_windows[i_ges][i_try][0]]
             del list_gestures[i_ges][i_try][i_ACTIVE_windows[i_ges][i_try][0]+i_ACTIVE_windows[i_ges][i_try][1]:]
-    if PRINT_TIME_CONSUMING: print("## ACTIVE_filter: %.2f" %(time.time()-t_ACTIVE_filter))
+    if PRINT_TIME_CONSUMING: print("## ACTIVE_filter: %.2f" %(time()-t_ACTIVE_filter))
     return np.array(list_gestures)
 
 def Repartition_N_Compute_RMS(ACTIVE_gestures, N=SEGMENT_N):
-    if PRINT_TIME_CONSUMING: t_Repartition_N_Compute_RMS=time.time()
+    if PRINT_TIME_CONSUMING: t_Repartition_N_Compute_RMS=time()
     # List all the data of each channel without partitioning into windows
     ACTIVE_N_gestures=[[[[] for i_ch in range(len(ACTIVE_gestures[0][0][0]))] for i_try in range(ACTIVE_gestures.shape[1])] for i_ges in range(ACTIVE_gestures.shape[0])]     # CONSTANT
     for i_ges in range(len(ACTIVE_gestures)):
@@ -144,39 +145,19 @@ def Repartition_N_Compute_RMS(ACTIVE_gestures, N=SEGMENT_N):
                     RMSs.append(compute_RMS(ACTIVE_N_gestures[i_ges][i_try][i_ch][(len(ACTIVE_N_gestures[i_ges][i_try][i_ch])//N)*i:(len(ACTIVE_N_gestures[i_ges][i_try][i_ch])//N)*(i+1)]))
                 ACTIVE_N_gestures[i_ges][i_try][i_ch]=np.array(RMSs)
             ACTIVE_N_gestures[i_ges][i_try]=np.array(ACTIVE_N_gestures[i_ges][i_try]).transpose()   # Change (4,10,168,N) -> (4,10,N,168)
-    if PRINT_TIME_CONSUMING: print("## Repartition_N_Compute_RMS: %.2f" %(time.time()-t_Repartition_N_Compute_RMS))
+    if PRINT_TIME_CONSUMING: print("## Repartition_N_Compute_RMS: %.2f" %(time()-t_Repartition_N_Compute_RMS))
     return np.array(ACTIVE_N_gestures)
 
 def mean_normalization(ACTIVE_N_RMS_gestures):
-    if PRINT_TIME_CONSUMING: t_mean_normalization=time.time()
+    if PRINT_TIME_CONSUMING: t_mean_normalization=time()
     for i_ges in range(len(ACTIVE_N_RMS_gestures)):
         for i_try in range(len(ACTIVE_N_RMS_gestures[i_ges])):
             for i_Lwin in range(len(ACTIVE_N_RMS_gestures[i_ges][i_try])):
                 delta=max(ACTIVE_N_RMS_gestures[i_ges][i_try][i_Lwin])-min(ACTIVE_N_RMS_gestures[i_ges][i_try][i_Lwin])
                 Mean=np.mean(ACTIVE_N_RMS_gestures[i_ges][i_try][i_Lwin])
                 ACTIVE_N_RMS_gestures[i_ges][i_try][i_Lwin]=(ACTIVE_N_RMS_gestures[i_ges][i_try][i_Lwin]-Mean)/delta
-    if PRINT_TIME_CONSUMING: print("## mean_normalization: %.2f" %(time.time()-t_mean_normalization))
+    if PRINT_TIME_CONSUMING: print("## mean_normalization: %.2f" %(time()-t_mean_normalization))
     return ACTIVE_N_RMS_gestures
-
-def construct_X_y(refined_data):
-    if PRINT_TIME_CONSUMING: t_construct_X_y=time.time()
-    X=np.reshape(refined_data, (refined_data.shape[0]*refined_data.shape[1]*refined_data.shape[2]*refined_data.shape[3], refined_data.shape[4]))
-    y=np.array([])
-    for i in range(refined_data.shape[0]):  # # of sessions
-        for i_ges in range(refined_data.shape[0]):
-            for j in range(refined_data.shape[1]):   # # of tries
-                for k in range(refined_data.shape[2]):  # # of Larege windows
-                    y=np.append(y, [i_ges])
-    if PRINT_TIME_CONSUMING: print("## construct_X_y: %.2f" %(time.time()-t_construct_X_y))
-    return X, y
-
-def plot_confusion_matrix(y_test, kinds, y_pred):
-    mat = confusion_matrix(y_test, y_pred)
-    sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False, xticklabels=kinds, yticklabels=kinds)
-    plt.xlabel('true label')
-    plt.ylabel('predicted label')
-    plt.axis('auto')
-    plt.show()
 
 def check_segment_len(i_ACTIVE_windows):
     for i in range(len(i_ACTIVE_windows)):
@@ -189,16 +170,16 @@ def plot_some_data(gestures):
     # Choose random three data
     chose=[]
     for i in range(3):
-        rand_ges = random.randint(1, len(gestures)-1)    # Except idle gesture
-        rand_try = random.randint(0, len(gestures[rand_ges])-1)
-        rand_win = random.randint(0, len(gestures[rand_ges][rand_try])-1)
+        rand_ges = randint(1, len(gestures)-1)    # Except idle gesture
+        rand_try = randint(0, len(gestures[rand_ges])-1)
+        rand_win = randint(0, len(gestures[rand_ges][rand_try])-1)
         chose.append((rand_ges, rand_try, rand_win))
     # Plot
     y,x=np.meshgrid(range(ACTUAL_RAW),range(ACTUAL_COLUMN))
     fig, ax = plt.subplots(nrows=3)
     im=[]
     for i in range(len(chose)):
-        df = pd.DataFrame({"x":x.flatten(), "y":y.flatten(),"value":gestures[chose[i][0]][chose[i][1]][chose[i][2]].flatten()}).pivot(index="y", columns="x", values="value")
+        df = DataFrame({"x":x.flatten(), "y":y.flatten(),"value":gestures[chose[i][0]][chose[i][1]][chose[i][2]].flatten()}).pivot(index="y", columns="x", values="value")
         im.append(ax[i].imshow(df.values, cmap="viridis", vmin=0, vmax=1))
         ax[i].set_title("%dth active window in %dth try in %dth gesture" %(chose[i][2], chose[i][1], chose[i][0]))
         fig.colorbar(im[i], ax=ax[i])
@@ -206,7 +187,7 @@ def plot_some_data(gestures):
     plt.show()
 
 def refined_data_for_each_session(pre_gestures):
-    if PRINT_TIME_CONSUMING: t_refined_data_for_each_session=time.time()
+    if PRINT_TIME_CONSUMING: t_refined_data_for_each_session=time()
     # Especially for Ref1, data reshaping into one array
     gestures=np.zeros((pre_gestures.shape[0], pre_gestures.shape[1])).tolist()      #CONSTANT
     for i_ges in range(len(pre_gestures)):
@@ -216,28 +197,28 @@ def refined_data_for_each_session(pre_gestures):
 
     # Signal Pre-processing & Construct windows
     ## Segmentation : Data processing : Discard_useless_data
-    if PRINT_TIME_CONSUMING: t_Discard_useless_data=time.time()
+    if PRINT_TIME_CONSUMING: t_Discard_useless_data=time()
     gestures=np.delete(gestures,np.s_[7:192:8],2)
-    if PRINT_TIME_CONSUMING: print("# Discard_useless_data: %.2f" %(time.time()-t_Discard_useless_data))
+    if PRINT_TIME_CONSUMING: print("# Discard_useless_data: %.2f" %(time()-t_Discard_useless_data))
     if PLOT_PRINT_PROCESSING: plot_ch(gestures, 3, 2, 50)
     ## Preprocessing : Apply_butterworth_band_pass_filter
-    if PRINT_TIME_CONSUMING: t_Apply_butterworth_band_pass_filter=time.time()
+    if PRINT_TIME_CONSUMING: t_Apply_butterworth_band_pass_filter=time()
     for i_ges in range(len(gestures)):
         for i_try in range(len(gestures[i_ges])):
             gestures[i_ges, i_try]=butter_bandpass_filter(gestures[i_ges, i_try])
-    if PRINT_TIME_CONSUMING: print("# Apply_butterworth_band_pass_filter: %.2f" %(time.time()-t_Apply_butterworth_band_pass_filter))
+    if PRINT_TIME_CONSUMING: print("# Apply_butterworth_band_pass_filter: %.2f" %(time()-t_Apply_butterworth_band_pass_filter))
     if PLOT_PRINT_PROCESSING: plot_ch(gestures, 3, 2, 50)
     ## Segmentation : Data processing : Divide_continuous_data_into_150_samples_window
-    if PRINT_TIME_CONSUMING: t_Divide_continuous_data_into_150_samples_window=time.time()
+    if PRINT_TIME_CONSUMING: t_Divide_continuous_data_into_150_samples_window=time()
     gestures=np.delete(gestures, np.s_[(gestures.shape[3]//WINDOW_SIZE)*WINDOW_SIZE:], 3)
     gestures=np.reshape(gestures,(gestures.shape[0], gestures.shape[1], gestures.shape[2], gestures.shape[3]//WINDOW_SIZE, WINDOW_SIZE))
-    if PRINT_TIME_CONSUMING: print("# Divide_continuous_data_into_150_samples_window: %.2f" %(time.time()-t_Divide_continuous_data_into_150_samples_window))
+    if PRINT_TIME_CONSUMING: print("# Divide_continuous_data_into_150_samples_window: %.2f" %(time()-t_Divide_continuous_data_into_150_samples_window))
 
     # Determine ACTIVE windows
     ## Segmentation : Compute_RMS
-    if PRINT_TIME_CONSUMING: t_Compute_RMS=time.time()
+    if PRINT_TIME_CONSUMING: t_Compute_RMS=time()
     RMS_gestures=compute_RMS_gestures(gestures)
-    if PRINT_TIME_CONSUMING: print("# Compute_RMS: %.2f" %(time.time()-t_Compute_RMS))
+    if PRINT_TIME_CONSUMING: print("# Compute_RMS: %.2f" %(time()-t_Compute_RMS))
     ## Segmentation : Base normalization
     if PLOT_PRINT_PROCESSING: 
         plt.imshow(RMS_gestures[3,2], cmap='hot_r', interpolation='nearest', vmin=0, vmax=0.0035)
@@ -247,9 +228,9 @@ def refined_data_for_each_session(pre_gestures):
         plt.imshow(RMS_gestures[3,2], cmap='hot_r', interpolation='nearest', vmin=0, vmax=0.0035)
         plt.show()
     ## Segmentation : Median_filtering
-    if PRINT_TIME_CONSUMING: t_Median_filtering=time.time()
+    if PRINT_TIME_CONSUMING: t_Median_filtering=time()
     RMS_gestures=np.apply_along_axis(medfilt, 3, RMS_gestures)
-    if PRINT_TIME_CONSUMING: print("# Median filtering: %.2f" %(time.time()-t_Median_filtering))
+    if PRINT_TIME_CONSUMING: print("# Median filtering: %.2f" %(time()-t_Median_filtering))
     if PLOT_PRINT_PROCESSING: 
         plt.imshow(RMS_gestures[3,2], cmap='hot_r', interpolation='nearest', vmin=0, vmax=0.0035)
         plt.show()
@@ -262,13 +243,11 @@ def refined_data_for_each_session(pre_gestures):
     ACTIVE_N_RMS_gestures=Repartition_N_Compute_RMS(ACTIVE_gestures)
     # Feature extraction : Mean normalization for all channels in each window
     mean_normalized_RMS=mean_normalization(ACTIVE_N_RMS_gestures)
-    
+
     # Plot one data
     if PLOT_RANDOM_DATA:
         plot_some_data(mean_normalized_RMS)
-
-
-    if PRINT_TIME_CONSUMING: print("refined_data_for_each_session: %.2f \n\n" %(time.time()-t_refined_data_for_each_session))
+    if PRINT_TIME_CONSUMING: print("refined_data_for_each_session: %.2f \n\n" %(time()-t_refined_data_for_each_session))
     return mean_normalized_RMS
 
 def plot_ch(data,i_gest,i_try=5,i_ch=89):
@@ -279,23 +258,41 @@ def plot_a_data(data):
     plt.imshow(data, cmap='hot_r', interpolation='nearest', vmin=0, vmax=0.0035)
     plt.show()
 
-def gnb_classifier(refined_data, TEST_RATIO=TEST_RATIO):
-    if PRINT_TIME_CONSUMING: t_gnb_classifier=time.time()
+def plot_confusion_matrix(y_test, kinds, y_pred):
+    mat = confusion_matrix(y_test, y_pred)
+    heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False, xticklabels=kinds, yticklabels=kinds)
+    plt.xlabel('true label')
+    plt.ylabel('predicted label')
+    plt.axis('auto')
+    plt.show()
 
-#########################################################################3
-    # Naive Bayes classifier : Construct X and y
+def construct_X_y(refined_data):
+    if PRINT_TIME_CONSUMING: t_construct_X_y=time()
+    X=np.reshape(refined_data, (refined_data.shape[0]*refined_data.shape[1]*refined_data.shape[2]*refined_data.shape[3], refined_data.shape[4]))
+    y=np.array([])
+    for i in range(refined_data.shape[0]):  # # of sessions
+        for i_ges in range(refined_data.shape[1]):
+            for j in range(refined_data.shape[2]):   # # of tries
+                for k in range(refined_data.shape[3]):  # # of Larege windows
+                    y=np.append(y, [i_ges])
+    if PRINT_TIME_CONSUMING: print("## construct_X_y: %.2f" %(time()-t_construct_X_y))
+    return X, y
+    
+def gnb_classifier(refined_data):
+    if PRINT_TIME_CONSUMING: t_gnb_classifier=time()
+    # Construct X and y
     X, y = construct_X_y(refined_data)
-#########################################################################
+    # Classifying
     gnb = GaussianNB()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_RATIO, random_state=0)
     y_pred = gnb.fit(X_train, y_train).predict(X_test)
     print("Accuracy : %d%%" % (100-(((y_test != y_pred).sum()/X_test.shape[0])*100)))
-    if PRINT_TIME_CONSUMING: print("#gnb_classifier: %.2f" %(time.time()-t_gnb_classifier))
+    if PRINT_TIME_CONSUMING: print("#gnb_classifier: %.2f" %(time()-t_gnb_classifier))
     if PLOT_CONFUSION_MATRIX:
         plot_confusion_matrix(y_test, list(set(y)), y_pred)
 
 def gmm_calibration(refined_data):
-    if PRINT_TIME_CONSUMING: t_gmm_calibration=time.time()
+    if PRINT_TIME_CONSUMING: t_gmm_calibration=time()
     """
     #interpolate
     y,x=np.meshgrid(range(ACTUAL_RAW),range(ACTUAL_COLUMN))
@@ -311,10 +308,10 @@ def gmm_calibration(refined_data):
     probs = gmm.predict_proba(X)
     print(probs[:5].round(3))
     """
-    if PRINT_TIME_CONSUMING: print("#gmm_calibration: %.2f" %(time.time()-t_gmm_calibration))
+    if PRINT_TIME_CONSUMING: print("#gmm_calibration: %.2f" %(time()-t_gmm_calibration))
 
 def main():
-    if PRINT_TIME_CONSUMING: t_main=time.time()
+    if PRINT_TIME_CONSUMING: t_main=time()
     sessions=load_mat_files("./data/")  # ndarray : sessions
     init_session=1
     for session in sessions.values():
@@ -327,9 +324,9 @@ def main():
         refined_data=np.append(refined_data, [refined_data_session], axis=0)
 
     # Calibraion : GMM method
-    if GMM_CLASSIFY: gmm_calibration(refined_data)
+    if GMM_CALIBRATE: gmm_calibration(refined_data)
     # Naive Bayes classifier : Basic method : NOT LOOCV
     if GNB_CLASSIFY: gnb_classifier(refined_data)
-    if PRINT_TIME_CONSUMING: print("main: %.2f" %(time.time()-t_main))
+    if PRINT_TIME_CONSUMING: print("main: %.2f" %(time()-t_main))
 
 main()
